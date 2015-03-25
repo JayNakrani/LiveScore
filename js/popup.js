4b03b8b6ba03e0443ff47ab62fffaadb21859159
Dhananjay Nakrani <dhananjaynakrani@gmail.com>
2015-03-24 18:59:58 -0700

    Add the code to update scores every minute.

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,35 @@
 /**
- * Whenever popup.html is loaded, display the scores.
+ * Global Vars
  */
-$(document).ready( function () {
-	//fetch the score from Cricinfo and display
+var interval_ref = 0;	//reference var to hold interval
+
+/**
+ * Updates the score. Fetches from Cricinfo and displays it.
+ */
+function updateScore()
+{
 	$.ajax({url: "http://static.cricinfo.com/rss/livescores.xml"})
 	 .done(function(data) {
 		$item = $(data).find("item");
 		$description = $item.find("description");
-  		
-  		$("#scoreWrap").html(($description.text()));
+
+		//hide loading text
+		$("#loadingWrap").addClass("hidden");
+
+		//display score!
+		$("#scoreWrap").html($description.text());
+		$("#scoreWrap").removeClass("hidden");
 	 });
+}
+
+/**
+ * Whenever popup.html is loaded, display the scores.
+ * Also set the interval so that it updates the score every minute.
+ */
+$(document).ready(function() {
+	//update score right now!
+	updateScore();
+
+	//Update the scores every minute.
+	interval_ref = setInterval(updateScore, 60*1000);
 });
